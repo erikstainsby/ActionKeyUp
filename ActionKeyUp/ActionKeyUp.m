87c853e778616af826f473d89e34a29938669abd
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-02-26 19:29:35 -0800

    Refactored RSTrixiePlugin/RSTrixieRule  to have a different implementation per subclass;  refactored RSTrixiePlugin/RSTrixiePlugin to have a different implementation for each subclass:  RSActionPlugin, RSReactionPlugin and RSConditionPlugin.

--- ActionKeyUp/ActionKeyUp.m
+++ ActionKeyUp/ActionKeyUp.m
@@ -14,13 +14,23 @@
 	
     if( nil != (self = [super initWithNibName:nibNameOrNil	bundle:nibBundleOrNil]))
     {
-		[self setName: @"KeyUp"];
+		[self setPluginName: @"KeyUp"];
     }
     return self;
 }
 
-- (BOOL) hasSelectorField {
-	return YES;
+- (BOOL) hasPreventDefaultButton { 
+	return YES; 
+} 
+- (BOOL) hasStopBubblingButton { 
+	return YES; 
+} 
+- (BOOL) preventDefault {
+	return [[self preventDefaultButton] state];
 }
+- (BOOL) stopBubbling {
+	return [[self stopBubblingButton] state];
+}
+
 
 @end
